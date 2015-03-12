e670dfa444a0e5fb79485144259fd18430985f77
liching21 <li_ching92@hotmail.com>
2015-03-12 21:07:36 +1100

    added Google Map

--- js/helper.js
+++ js/helper.js
@@ -64,8 +64,8 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+    var iName = inName(bio.name) || function(){};
+    $('#name').html(iName);
   });
 });
 
@@ -85,10 +85,9 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
+  logClicks(loc.pageX,loc.pageY);
+
 });
-
-
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
@@ -232,12 +231,12 @@ function initializeMap() {
 Uncomment the code below when you're ready to implement a Google Map!
 */
 
-// Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+//Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   // Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,4 @@
-/** 										**/	
+/** 										**/
 /*	File: ResumeBuilder.js 					 */
 /*	Based on: JavaScript Basics from Udacity */
 /*	Written by: Liching Yew					 */
@@ -13,7 +13,7 @@ var bio = {
 		"github" : "https://github.com/liching21",
 		"location" : "Sydney, Australia"
 	},
-	
+
 	"pictureURL" : "images/may-main.jpg",
 	"welcomeMessage" : "Hello everyone, welcome to my portfolio page!",
 	"skills" : ["HTML" , " JS" , " CSS" , " Responsive"]
@@ -77,7 +77,7 @@ var projects = {
 			"dates" : "February 2015",
 			"description" : "A web page reflecting my life event during th year I was 22",
 			"images" : [
-				"images/22img1.png", 
+				"images/22img1.png",
 				"images/22img2.png"
 			]
 		},
@@ -86,12 +86,39 @@ var projects = {
 			"dates" : "February 2014 - January 2015",
 			"description" : "Website for the Sydney Emeralds Synchro Club",
 			"images" : [
-				"images/combo-states.jpg", 
+				"images/combo-states.jpg",
 				"images/competitive-lift.jpg"
 			]
 		}
 	]
 }
+
+/** added the display method for the projects */
+
+projects.display = function(){
+
+		/** loop through each project and add info */
+		for (var project in projects.projects){
+
+			$("#projects").append(HTMLprojectStart);
+
+			var formattedProjectTitle = HTMLprojectTitle.replace("%data%",projects.projects[project].title);
+			var formattedProjectDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+			var formattedProjectDescription = HTMLprojectDescription.replace("%data%",projects.projects[project].description);
+
+			$(".project-entry:last").append(formattedProjectTitle);
+			$(".project-entry:last").append(formattedProjectDates);
+			$(".project-entry:last").append(formattedProjectDescription);
+
+			for (var img in projects.projects[project].images){
+				var formattedProjectImage = HTMLprojectImage.replace("%data%",projects.projects[project].images[img]);
+				$(".project-entry:last").append(formattedProjectImage);
+			}
+		}
+	}
+
+/** calling display projects function */
+projects.display();
 
 /** Creating Education object with brackets notation **/
 /**
@@ -126,8 +153,8 @@ var education = {
 	  		"title" : "Front End Web Development",
 		  	"school" : "Udacity",
 		  	"dates" : "Feb 2015 - Ongoing"
-	  	}	
-	  ] 
+	  	}
+	  ]
 }
 
 /** write an if statement to check if there are skills in the bio object **/
@@ -151,28 +178,69 @@ if(bio.skills.length > 0){
 
 /** write a for in loop that iterates over all jobs in the work obj **/
 /*	format each jobs employer	and job title													 */
-var formattedEmployer;
-var formattedTitle;
-var combineEmployerTitle;
 
-for (job in work.jobs){
 
-	formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
-	formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-	formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].location);
-	formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].dates);
-	formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-	
-	combineEmployerTitle = formattedEmployer + formattedTitle;
+/** create a displayWork() function that displays my work */
 
-	$("#workExperience").append(HTMLworkStart);
-	$("#workExperience:last").append(combineEmployerTitle);
-	$("#workExperience:last").append(formattedDates);
-	$("#workExperience:last").append(formattedLocation);
-	$("#workExperience:last").append(formattedDescription);
+var displayWork = function(){
+	var formattedEmployer;
+	var formattedTitle;
+	var combineEmployerTitle;
 
-	console.log(work.jobs[job].employer, work.jobs[job].title);
+	for (job in work.jobs){
+
+		formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+		formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].location);
+		formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].dates);
+		formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+		combineEmployerTitle = formattedEmployer + formattedTitle;
+
+		$("#workExperience").append(HTMLworkStart);
+		$("#workExperience:last").append(combineEmployerTitle);
+		$("#workExperience:last").append(formattedDates);
+		$("#workExperience:last").append(formattedLocation);
+		$("#workExperience:last").append(formattedDescription);
+
+		console.log(work.jobs[job].employer, work.jobs[job].title);
+	}
 }
+
+displayWork();
+
+
+/** Lesson 2 internationalise the name */
+$("#main").append(internationalizeButton);
+
+function inName(name) {
+	var names = name.split(" ");
+	names[1] = names[1].toUpperCase();
+	names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+	var joinName = names.join(" ");
+	return joinName;
+}
+
+/** show google map */
+$("#mapDiv").append(googleMap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
