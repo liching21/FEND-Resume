cc145e7ce3f833be5043a82ca05eb22d9d427a0b
liching21 <li_ching92@hotmail.com>
2015-05-31 13:16:15 +1000

    Revert "minify css and js files"
    This reverts commit de76601e3405061cb65557321640480dda573865.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,35 +5,18 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         uglify:{
-            my_target: {
-              files: {
-                // desc: src
-                'js/build/jQuery.min.js': 'js/jQuery.js',
-                'js/build/helper.min.js' : 'js/helper.js',
-                'js/build/resumeBuilder.min.js' : 'js/resumeBuilder.js'
-              }
+            build: {
+                src: ['js/jQuery.js', 'js/helper.js'], //input
+                dest: 'js/build/global.min.js' //output
             }
-        },
-
-        cssmin: {
-          options: {
-            shorthandCompacting: false,
-            roundingPrecision: -1
-          },
-          target: {
-            files: {
-              'css/build/style.min.css': 'css/style.css'
-            }
-          }
         }
 
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['uglify','cssmin']);
+    grunt.registerTask('default', ['uglify']);
 
 };
